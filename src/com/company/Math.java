bc9971203c538cbdf0c381c989ad62b30c0ec582
SofieBC <fikonglass@hotmail.com>
2019-12-03 11:35:40 +0100

    Added add method to the math class

--- src/com/company/Math.java
+++ src/com/company/Math.java
@@ -1,4 +1,13 @@
 package com.company;
 
 public class Math {
-}
+
+    public static void main(String[] args) {
+    }
+
+
+    public int add (int first, int second){
+        return first + second;
+    }
+
+    }
